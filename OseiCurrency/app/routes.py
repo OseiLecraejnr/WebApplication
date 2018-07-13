4545c018c2843c8a55498d1f2fa4e407adae9363
OseiLecraejnr <oseidesantosjnr@gmail.com>
2018-07-13 16:39:50 +0000

    new file

--- OseiCurrency/app/routes.py
+++ OseiCurrency/app/routes.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import requests
 import json
@@ -56,4 +57,5 @@ def res():
 	
  
 if __name__ == '__main__':
-  app.run(debug=True)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host='0.0.0.0', port=port)
